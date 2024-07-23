1180da3d66ac2b7fcf3317e671c24e1fd9036b06
Nabil Sabi <nabilsabi01@gmail.com>
2024-07-23 10:03:22 +0100

    project structure

---
+++ src/main/java/com/devart/helloevents/HelloEventsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devart.helloevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloEventsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEventsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devart/helloevents/HelloEventsBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.devart.helloevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEventsBackendApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+
+}
