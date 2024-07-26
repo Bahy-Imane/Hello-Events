2eac3e56b925efcd36d8ba06de68efaf73e63353
Nabil Sabi <nabilsabi01@gmail.com>
2024-07-26 15:51:22 +0100

    fix problem in events

--- src/main/java/com/devart/helloevents/repository/EventRepository.java
+++ src/main/java/com/devart/helloevents/repository/EventRepository.java
@@ -2,6 +2,8 @@ package com.devart.helloevents.repository;
 
 import com.devart.helloevents.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
--- src/main/java/com/devart/helloevents/repository/UserRepository.java
+++ src/main/java/com/devart/helloevents/repository/UserRepository.java
@@ -2,14 +2,13 @@ package com.devart.helloevents.repository;
 
 import com.devart.helloevents.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-Optional<User> findUsersByuserNameOrEmail(String username, String email);
-
-boolean existsByEmail(String email);
-
-boolean existsByUserName(String userName);
+    Optional<User> findUsersByuserNameOrEmail(String username, String email);
+    boolean existsByEmail(String email);
+    boolean existsByUserName(String userName);
 }
